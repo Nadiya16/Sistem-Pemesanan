6c94d1bb72d74ab7849f2c9fe0d011fbe062632f
MNurIskandar <ikan66847@gmail.com>
2024-09-05 14:43:45 +0700

    #1 upload project user

---
+++ src/koneksi.java
@@ -0,0 +1,28 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author M. Nur Iskandar
+ */
+public class koneksi {
+    private static Connection conn;
+    
+    public static Connection getConnection() throws SQLException {
+        if (conn == null || conn.isClosed()) {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sistem_penjualan", "root", "");
+        } catch(ClassNotFoundException | SQLException e){
+            e.printStackTrace();
+            throw new SQLException("Gagal menghubugkan ke database", e);
+        }
+    }
+    return conn;
+    }
+    
+}
+
+    
